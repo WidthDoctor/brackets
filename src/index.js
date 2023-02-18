115b94224ad93f0034e17d41eda91509919b7b7c
WidthDoctor <elskel1812@gmail.com>
2023-02-18 13:47:31 +0100

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let soup = 1;
+  while (soup === 1) {
+      let OldStr = str;
+      bracketsConfig.map((del) => (str = str.replace(del[0] + del[1], "")));
+      if (OldStr === str) {
+          soup = 0;
+      }
+  }
+  return str.length === 0 ? true : false;
+};
